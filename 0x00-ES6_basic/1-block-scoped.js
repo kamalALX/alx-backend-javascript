3fd93675f34625ce369ba1d1985257faa09024c8
KamalALX <kamalmiftah01@gmail.com>
2024-07-22 22:44:47 +0100

    solving task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export default function taskBlock(trueOrFalse) {
+  var task;
+  var task2;
+
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+
+  task = false;
+  task2 = true;
+
+  return [task, task2];
+}
